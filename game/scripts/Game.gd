feadf3b0d5555866083ac6887125f1fd734007f1
Lucas de Oliveira Reis <lucas.reis@iMac-de-Lucas.local>
2017-10-24 01:35:54 -0200

    Changing weapon to another when smashed complete

--- game/scripts/Game.gd
+++ game/scripts/Game.gd
@@ -1,10 +1,23 @@
 extends Node2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+onready var weapon = preload("res://scenes/weapons/RawWeapon.tscn")
+
+onready var weapons_group = get_node("Weapons")
+onready var current_weapon = get_node("Weapons/RawWeapon")
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
 	pass
+
+func _on_RawWeapon_weapon_ready():
+	var new_weapon = weapon.instance()
+	weapons_group.add_child(new_weapon)
+
+	new_weapon.set_pos(current_weapon.get_pos())
+	# change to next job on the list
+	new_weapon.init(load("res://assets/weapons/knife.png"), 5, "knife")
+	
+	current_weapon.destroy()
+	current_weapon = new_weapon
+
+func _on_Timer_timeout():
+	print(current_weapon.name)
--- game/scripts/RawWeapon.gd
+++ game/scripts/RawWeapon.gd
@@ -1,8 +1,28 @@
 extends Node2D
 
 onready var sprite = get_node("Sprite")
+onready var anim = get_node("Anim")
+
+var name = "axe"
+var hits_before_complete = 1
+
+signal weapon_ready
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
 	pass
+
+func init(sprite_texture, hits_before_complete, name):
+	self.sprite.set_texture(sprite_texture)
+	self.hits_before_complete = hits_before_complete
+	self.name = name
+
+func destroy():
+	queue_free()
+
+func _on_PlayerBar_hitted(isHit):
+	if isHit and hits_before_complete > 0:
+		anim.play("Vibrate")
+		hits_before_complete -= 1
+		
+	if hits_before_complete == 0:
+		emit_signal("weapon_ready")
