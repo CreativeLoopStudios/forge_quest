0a0dc96ccaa95a0390a8879cf86c3b0509a2a536
Lucas de Oliveira Reis <lucas.reis@iMac-de-Lucas.local>
2017-10-31 02:03:06 -0200

    Code to change the weapon in play

--- game/scripts/Game.gd
+++ game/scripts/Game.gd
@@ -1,23 +1,22 @@
 extends Node2D
 
-onready var weapon = preload("res://scenes/weapons/RawWeapon.tscn")
+onready var weapon_scn = preload("res://scenes/weapons/RawWeapon.tscn")
+onready var raw_weapon = get_node("Weapons/RawWeapon")
 
-onready var weapons_group = get_node("Weapons")
-onready var current_weapon = get_node("Weapons/RawWeapon")
+const AXE = "axe"
+const KNIFE = "knife"
+
+var index_current_weapon = 0
+var weapons = []
 
 func _ready():
-	pass
+	weapons.append(AXE)
+	weapons.append(KNIFE)
+	self._on_RawWeapon_weapon_ready()
 
 func _on_RawWeapon_weapon_ready():
-	var new_weapon = weapon.instance()
-	weapons_group.add_child(new_weapon)
-
-	new_weapon.set_pos(current_weapon.get_pos())
-	# change to next job on the list
-	new_weapon.init(load("res://assets/weapons/knife.png"), 5, "knife")
+	if index_current_weapon == weapons.size(): return
 	
-	current_weapon.destroy()
-	current_weapon = new_weapon
-
-func _on_Timer_timeout():
-	print(current_weapon.name)
+	var current_weapon_name = weapons[index_current_weapon]
+	raw_weapon.init(current_weapon_name, 1)
+	index_current_weapon += 1
--- game/scripts/RawWeapon.gd
+++ game/scripts/RawWeapon.gd
@@ -3,18 +3,18 @@ extends Node2D
 onready var sprite = get_node("Sprite")
 onready var anim = get_node("Anim")
 
-var name = "axe"
-var hits_before_complete = 1
+var texture_name
+var hits_before_complete = 0
 
 signal weapon_ready
 
 func _ready():
 	pass
 
-func init(sprite_texture, hits_before_complete, name):
-	self.sprite.set_texture(sprite_texture)
+func init(texture_name, hits_before_complete):
+	self.texture_name = texture_name
+	self.sprite.set_texture(load("res://assets/weapons/" + texture_name + ".png"))
 	self.hits_before_complete = hits_before_complete
-	self.name = name
 
 func destroy():
 	queue_free()
@@ -22,7 +22,9 @@ func destroy():
 func _on_PlayerBar_hitted(isHit):
 	if isHit and hits_before_complete > 0:
 		anim.play("Vibrate")
-		hits_before_complete -= 1
-		
+
+func _on_Anim_finished():
+	hits_before_complete -= 1
+	
 	if hits_before_complete == 0:
 		emit_signal("weapon_ready")
